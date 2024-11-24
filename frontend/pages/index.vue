c32ac29e4d818ce41fd7346051c31ac7cea72c3e
Miłosz Bolewski <milosz.bolewski.33@gmail.com>
2024-11-24 09:20:59 +0100

    Remove redundant packages

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,14 +1,5 @@
 <template>
   <h1>Index page</h1>
-  <v-btn
-    @click="generateInvoice"
-    :disabled="isGeneratingInvoice"
-    >Generate invoice</v-btn
-  >
 </template>
 
-<script lang="ts" setup>
-import { useInvoices } from "~/composables/useInvoices";
-
-const { generateInvoice, isGeneratingInvoice } = useInvoices();
-</script>
+<script lang="ts" setup></script>

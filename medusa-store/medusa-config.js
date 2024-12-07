a94efeb92524fdf1f8c724fb61ebfeacdb61bf30
Miłosz Bolewski <milosz.bolewski.33@gmail.com>
2024-12-07 11:42:54 +0100

    switch back env file for production

--- medusa-store/medusa-config.js
+++ medusa-store/medusa-config.js
@@ -3,7 +3,7 @@ const dotenv = require("dotenv");
 let ENV_FILE_NAME = "";
 switch (process.env.NODE_ENV) {
   case "production":
-    ENV_FILE_NAME = ".env";
+    ENV_FILE_NAME = ".env.production";
     break;
   case "staging":
     ENV_FILE_NAME = ".env.staging";
